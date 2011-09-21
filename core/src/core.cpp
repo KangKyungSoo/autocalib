7dc3ef739145b212155317504e1a73c5d24e42dc
alexey <alexey@localhost>
2011-09-21 22:40:54 +0300

    Minor

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 
 namespace autocalib {
 
-void calibRotationalCamera(cv::InputArrayOfArrays keypoints,
+void CalibRotationalCamera(cv::InputArrayOfArrays keypoints,
                            const std::map<std::pair<int, int>, cv::Mat> &matches,
                            cv::InputOutputArray K,
                            cv::InputArray mask)
