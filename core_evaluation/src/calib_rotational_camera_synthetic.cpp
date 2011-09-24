69eb8dfabae1c24b61abdb31fbfaf5346a26d73e
alexey <alexey@alexey-notebook>
2011-09-24 22:41:52 +0300

    Added core_evaluation folder

---
+++ core_evaluation/src/calib_rotational_camera_synthetic.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <core/include/core.h>
+#include <evaluation/include/evaluation.h>
+
+using namespace cv;
+using namespace autocalib;
+
+int main(int argc, char **argv) {
+    return 0;
+}
