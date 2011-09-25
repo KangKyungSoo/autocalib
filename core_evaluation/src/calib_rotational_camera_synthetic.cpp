c0c69376fc97d8095e2e52e8be05b5daa62c9824
alexey <alexey@localhost>
2011-09-25 20:21:24 +0300

    Added logging supprt

--- core_evaluation/src/calib_rotational_camera_synthetic.cpp
+++ core_evaluation/src/calib_rotational_camera_synthetic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdexcept>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <core/include/core.h>
@@ -19,110 +20,113 @@ double max_angle = 0.1;
 bool create_images = false;
 
 int main(int argc, char **argv) {    
-    for (int i = 1; i < argc; ++i) {
-        if (string(argv[i]) == "--num-points")
-            num_points = atoi(argv[++i]);
-        else if (string(argv[i]) == "--num-cameras")
-            num_cameras = atoi(argv[++i]);
-        else if (string(argv[i]) == "--viewport") {
-            viewport = Rect(atoi(argv[i + 1]), atoi(argv[i + 2]),
-                            atoi(argv[i + 3]), atoi(argv[i + 4]));
-            i += 4;
+    try {
+        for (int i = 1; i < argc; ++i) {
+            if (string(argv[i]) == "--num-points")
+                num_points = atoi(argv[++i]);
+            else if (string(argv[i]) == "--num-cameras")
+                num_cameras = atoi(argv[++i]);
+            else if (string(argv[i]) == "--viewport") {
+                viewport = Rect(atoi(argv[i + 1]), atoi(argv[i + 2]),
+                                atoi(argv[i + 3]), atoi(argv[i + 4]));
+                i += 4;
+            }
+            else if (string(argv[i]) == "--K-gold") {
+                K_gold = Mat::eye(3, 3, CV_64F);
+                K_gold(0, 0) = atof(argv[i + 1]);
+                K_gold(0, 1) = atof(argv[i + 2]);
+                K_gold(0, 2) = atof(argv[i + 3]);
+                K_gold(1, 1) = atof(argv[i + 4]);
+                K_gold(1, 2) = atof(argv[i + 5]);
+            }
+            else if (string(argv[i]) == "--camera-center") {
+                camera_center = Mat::zeros(3, 1, CV_64F);
+                camera_center(0, 0) = atof(argv[i + 1]);
+                camera_center(1, 0) = atof(argv[i + 2]);
+                camera_center(2, 0) = atof(argv[i + 3]);
+                i += 3;
+            }
+            else if (string(argv[i]) == "--max-angle")
+                max_angle = atof(argv[++i]);
+            else if (string(argv[i]) == "--create-images")
+                create_images = atoi(argv[++i]);
+            else
+                throw runtime_error("Can't parse CLI args");
         }
-        else if (string(argv[i]) == "--K-gold") {
+        if (K_gold.empty()) {
             K_gold = Mat::eye(3, 3, CV_64F);
-            K_gold(0, 0) = atof(argv[i + 1]);
-            K_gold(0, 1) = atof(argv[i + 2]);
-            K_gold(0, 2) = atof(argv[i + 3]);
-            K_gold(1, 1) = atof(argv[i + 4]);
-            K_gold(1, 2) = atof(argv[i + 5]);
+            K_gold(0, 0) = K_gold(1, 1) = viewport.width + viewport.height;
+            K_gold(0, 2) = viewport.width * 0.5;
+            K_gold(1, 2) = viewport.height * 0.5;
         }
-        else if (string(argv[i]) == "--camera-center") {
+        if (camera_center.empty()) {
             camera_center = Mat::zeros(3, 1, CV_64F);
-            camera_center(0, 0) = atof(argv[i + 1]);
-            camera_center(1, 0) = atof(argv[i + 2]);
-            camera_center(2, 0) = atof(argv[i + 3]);
-            i += 3;
+            camera_center(2, 0) = -10;
         }
-        else if (string(argv[i]) == "--max-angle")
-            max_angle = atof(argv[++i]);
-        else if (string(argv[i]) == "--create-images")
-            create_images = atoi(argv[++i]);
-        else {
-            cout << "Can't parse CLI args\n";
-            return -1;
-        }
-    }
-    if (K_gold.empty()) {
-        K_gold = Mat::eye(3, 3, CV_64F);
-        K_gold(0, 0) = K_gold(1, 1) = viewport.width + viewport.height;
-        K_gold(0, 2) = viewport.width * 0.5;
-        K_gold(1, 2) = viewport.height * 0.5;
-    }
-    if (camera_center.empty()) {
-        camera_center = Mat::zeros(3, 1, CV_64F);
-        camera_center(2, 0) = -10;
-    }
 
-    Ptr<SyntheticScene> scene = new SphereScene(num_points);
-    vector<RigidCamera> cameras(num_cameras);
-    vector<Ptr<detail::ImageFeatures> > features(num_cameras);
+        Ptr<SyntheticScene> scene = new SphereScene(num_points);
+        vector<RigidCamera> cameras(num_cameras);
+        vector<Ptr<detail::ImageFeatures> > features(num_cameras);
 
-    // Generate cameras
-    for (int i = 0; i < num_cameras; ++i) {
-        Mat rvec = Mat::zeros(3, 1, CV_64F);
-        randu(rvec, -1, 1);
-        rvec /= norm(rvec) / (rand() / (double)RAND_MAX * max_angle);
+        // Generate cameras
+        for (int i = 0; i < num_cameras; ++i) {
+            Mat rvec = Mat::zeros(3, 1, CV_64F);
+            randu(rvec, -1, 1);
+            rvec /= norm(rvec) / (rand() / (double)RAND_MAX * max_angle);
 
-        Mat R;
-        Rodrigues(rvec, R);
-        cameras[i] = RigidCamera::LocalToWorld(K_gold, R, camera_center);
+            Mat R;
+            Rodrigues(rvec, R);
+            cameras[i] = RigidCamera::LocalToWorld(K_gold, R, camera_center);
 
-        if (!create_images)
-            features[i] = scene->TakeShot(cameras[i], viewport);
-        else {
-            Mat image;
-            features[i] = scene->TakeShot(cameras[i], viewport, &image);
-            stringstream name;
-            name << "camera" << i << ".jpg";
-            imwrite(name.str(), image);
-        }
-    }
-
-    // Find homographies
-    vector<Mat> Hs;
-    Mat kps1, kps2;
-    vector<DMatch> matches;
-    for (int i = 0; i < num_cameras - 1; ++i) {
-        for (int j = i + 1; j < num_cameras; ++j) {
-            MatchSyntheticShots((*features[i]), (*features[j]), matches);
-            ExtractMatchedKeypoints((*features[i]), (*features[j]), matches, kps1, kps2);
-            Mat_<double> H = findHomography(kps1, kps2);
-            if (H.empty())
-                cout << "Can't find H from " << i << " to " << j << endl;
+            if (!create_images)
+                features[i] = scene->TakeShot(cameras[i], viewport);
             else {
-                Hs.push_back(H);
-
-                // Calcuate reprojection error
-                double err = 0;
-                for (size_t k = 0; k < matches.size(); ++k) {
-                    Point2f kp1 = kps1.at<Point2f>(0, k);
-                    Point2f kp2 = kps2.at<Point2f>(0, k);
-                    double x = H(0, 0) * kp1.x + H(0, 1) * kp1.y + H(0, 2);
-                    double y = H(1, 0) * kp1.x + H(1, 1) * kp1.y + H(1, 2);
-                    double z = H(2, 0) * kp1.x + H(2, 1) * kp1.y + H(2, 2);
-                    err += (kp2.x - x / z) * (kp2.x - x / z) + (kp2.y - y / z) * (kp2.y - y / z);
-                }
-                cout << "H from " << i << " to " << j << " RMS error: " << sqrt(err / matches.size()) << endl;
+                Mat image;
+                features[i] = scene->TakeShot(cameras[i], viewport, &image);
+                stringstream name;
+                name << "camera" << i << ".jpg";
+                imwrite(name.str(), image);
             }
         }
+
+        // Find homographies
+        vector<Mat> Hs;
+        Mat kps1, kps2;
+        vector<DMatch> matches;
+        for (int i = 0; i < num_cameras - 1; ++i) {
+            for (int j = i + 1; j < num_cameras; ++j) {
+                MatchSyntheticShots((*features[i]), (*features[j]), matches);
+                ExtractMatchedKeypoints((*features[i]), (*features[j]), matches, kps1, kps2);
+                Mat_<double> H = findHomography(kps1, kps2);
+                if (H.empty())
+                    cout << "Can't find H from " << i << " to " << j << endl;
+                else {
+                    Hs.push_back(H);
+
+                    // Calcuate reprojection error
+                    double err = 0;
+                    for (size_t k = 0; k < matches.size(); ++k) {
+                        Point2f kp1 = kps1.at<Point2f>(0, k);
+                        Point2f kp2 = kps2.at<Point2f>(0, k);
+                        double x = H(0, 0) * kp1.x + H(0, 1) * kp1.y + H(0, 2);
+                        double y = H(1, 0) * kp1.x + H(1, 1) * kp1.y + H(1, 2);
+                        double z = H(2, 0) * kp1.x + H(2, 1) * kp1.y + H(2, 2);
+                        err += (kp2.x - x / z) * (kp2.x - x / z) + (kp2.y - y / z) * (kp2.y - y / z);
+                    }
+                    cout << "H from " << i << " to " << j << " RMS error: " << sqrt(err / matches.size()) << endl;
+                }
+            }
+        }
+
+        cout << "Linear calibrating...\n";
+        Mat_<double> K_final = CalibRotationalCameraLinear(Hs);
+
+        cout << "K_gold:\n" << K_gold << endl;
+        cout << "K_final:\n" << K_final << endl;
     }
-
-    cout << "Linear calibrating...\n";
-    Mat_<double> K_final = CalibRotationalCameraLinear(Hs);
-
-    cout << "K_gold:\n" << K_gold << endl;
-    cout << "K_final:\n" << K_final << endl;
+    catch (const exception &e) {
+        cout << "Error: '" << e.what() << "'\n";
+    }
 
     return 0;
 }
