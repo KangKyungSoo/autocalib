69f8ec3ee6d860694e016b2d74efd4f17c03006b
alexey <alexey@localhost>
2011-10-12 22:29:04 +0300

    Some

--- core_evaluation/src/calib_R_cam_real.cpp
+++ core_evaluation/src/calib_R_cam_real.cpp
@@ -2,23 +2,78 @@
 #include <vector>
 #include <stdexcept>
 #include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/stitching/detail/matchers.hpp>
 #include <core/include/core.h>
 
 using namespace std;
 using namespace cv;
 using namespace autocalib;
 
-vector<Mat> imgs;
 
-void ParseArgs(int argc, char **argv) {
-    for (int i = 1; i < argc; ++i) {
-        Mat img = imread(argv[++i]);
-        if (img.empty())
-            throw runtime_error(string("Can't open image: ") + argv[i]);
+/** Describes an ORB features finder. */
+class OrbFeaturesFinder : public detail::FeaturesFinder {
+public:
+
+    /** Constructs ORB features finder.
+      *
+      * \param num_features Number of desired features
+      */
+    OrbFeaturesFinder(int num_features) : orb_(num_features) {}
+
+private:
+    virtual void find(const Mat &image, detail::ImageFeatures &features) {
+        orb_(image, Mat(), features.keypoints, features.descriptors);
     }
-}
 
+    ORB orb_;
+};
+
+
+/** Base class for features finder creators */
+class FeaturesFinderCreator {
+public:
+    virtual ~FeaturesFinderCreator() {}
+
+    /** Creates a features finder.
+      *
+      * \return Pointer to features finder object
+      */
+    virtual Ptr<detail::FeaturesFinder> Create() = 0;
+};
+
+
+class SurfFeaturesFinderCreator : public FeaturesFinderCreator {
+public:
+    SurfFeaturesFinderCreator() : hess_thresh(300), num_octaves(3), num_layers(4) {}
+
+    virtual Ptr<detail::FeaturesFinder> Create() {
+        return new detail::SurfFeaturesFinder(hess_thresh, num_octaves, num_layers);
+    }
+
+    double hess_thresh;
+    int num_octaves;
+    int num_layers;
+};
+
+
+class OrbFeaturesFinderCreator : public FeaturesFinderCreator {
+public:
+    OrbFeaturesFinderCreator() : num_features(500) {}
+
+    virtual Ptr<detail::FeaturesFinder> Create() {
+        return new OrbFeaturesFinder(num_features);
+    }
+
+    int num_features;
+};
+
+
+void ParseArgs(int argc, char **argv);
+
+vector<Mat> imgs;
+Ptr<FeaturesFinderCreator> finder_creator = new SurfFeaturesFinderCreator();
 
 int main(int argc, char **argv) {
     try {
@@ -28,4 +83,13 @@ int main(int argc, char **argv) {
         cout << "Error: " << e.what() << endl;
     }
     return 0;
+}
+
+
+void ParseArgs(int argc, char **argv) {
+    for (int i = 1; i < argc; ++i) {
+        Mat img = imread(argv[++i]);
+        if (img.empty())
+            throw runtime_error(string("Can't open image: ") + argv[i]);
+    }
 }
