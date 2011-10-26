cb1a7468f1e737bc4ba7feced0e15226200fd394
alexey <alexey@localhost>
2011-10-26 13:59:15 +0400

    Added more tests for DLT triangulation

--- core_tests/src/core.cpp
+++ core_tests/src/core.cpp
@@ -53,13 +53,15 @@ TEST(DecomposeUUt, CanDecomposeSmallMatrix) {
 }
 
 
-TEST(DltTraingulation, CanTriangluateSphere) {
+class DltTriangulationMetric : public testing::TestWithParam<Ptr<PointCloudSceneCreator> > { };
+
+TEST_P(DltTriangulationMetric, CanTriangulate) {
     RNG rng(0);
     int num_points = 1000;
     double max_angle = 0.1;
     Rect viewport = Rect(0, 0, 640, 480);
 
-    Ptr<PointCloudScene> scene = new SphereScene(num_points, rng);
+    Ptr<PointCloudScene> scene = GetParam()->Create(num_points, rng);
 
     Mat_<double> K = Mat::eye(3, 3, CV_64F);
     K(0, 0) = K(1, 1) = viewport.width + viewport.height;
@@ -112,14 +114,20 @@ TEST(DltTraingulation, CanTriangluateSphere) {
     }
 }
 
+INSTANTIATE_TEST_CASE_P(StdSynthScenes,
+                        DltTriangulationMetric,
+                        testing::Values(new SphereSceneCreator(), new CubeSceneCreator()));
 
-TEST(DltTraingulation, CanTriangluateCube) {
+
+class DltTriangulationProjective : public testing::TestWithParam<Ptr<PointCloudSceneCreator> > { };
+
+TEST_P(DltTriangulationProjective, CanTriangulate) {
     RNG rng(0);
     int num_points = 1000;
     double max_angle = 0.1;
-    Rect viewport = Rect(0, 0, 640, 480);
+    Rect viewport = Rect(0, 0, 640, 480);    
 
-    Ptr<PointCloudScene> scene = new CubeScene(num_points, rng);
+    Ptr<PointCloudScene> scene = GetParam()->Create(num_points, rng);
 
     Mat_<double> K = Mat::eye(3, 3, CV_64F);
     K(0, 0) = K(1, 1) = viewport.width + viewport.height;
@@ -154,21 +162,37 @@ TEST(DltTraingulation, CanTriangluateCube) {
         xy1(0, 2 * i + 1) = f1.keypoints[matches[i].trainIdx].pt.y;
     }
 
+    Mat_<double> H = Mat::zeros(4, 4, CV_64F);
+    while (abs(determinant(H)) < 1e-3)
+        rng.fill(H, RNG::UNIFORM, 0, 1);
+    H /= pow(abs(determinant(H)), 0.25);
+    Mat_<double> H_inv = H.inv();
+
     DltTriangulation dlt;
 
+    Mat_<double> P0 = cameras[0].P() * H_inv;
+    Mat_<double> P1 = cameras[1].P() * H_inv;
+
     Mat_<double> xyzw;
-    dlt.triangulate(cameras[0], cameras[1], xy0, xy1, xyzw);
+    dlt.triangulate(ProjectiveCamera(P0), ProjectiveCamera(P1), xy0, xy1, xyzw);
 
     for (size_t i = 0; i < matches.size(); ++i) {
-        int point_idx = f0.descriptors.at<int>(matches[i].queryIdx);
-        Point3d point_gold = scene->localPointAt(point_idx);
-        Point3d point_found(xyzw(0, 4 * i) / xyzw(0, 4 * i + 3),
-                            xyzw(0, 4 * i + 1) / xyzw(0, 4 * i + 3),
-                            xyzw(0, 4 * i + 2) / xyzw(0, 4 * i + 3));
+        double x0 = P0(0, 0) * xyzw(0, 4 * i) + P0(0, 1) * xyzw(0, 4 * i + 1) + P0(0, 2) * xyzw(0, 4 * i + 2) + P0(0, 3) * xyzw(0, 4 * i + 3);
+        double y0 = P0(1, 0) * xyzw(0, 4 * i) + P0(1, 1) * xyzw(0, 4 * i + 1) + P0(1, 2) * xyzw(0, 4 * i + 2) + P0(1, 3) * xyzw(0, 4 * i + 3);
+        double z0 = P0(2, 0) * xyzw(0, 4 * i) + P0(2, 1) * xyzw(0, 4 * i + 1) + P0(2, 2) * xyzw(0, 4 * i + 2) + P0(2, 3) * xyzw(0, 4 * i + 3);
 
-        ASSERT_NEAR(point_gold.x, point_found.x, 1e-5);
-        ASSERT_NEAR(point_gold.y, point_found.y, 1e-5);
-        ASSERT_NEAR(point_gold.z, point_found.z, 1e-5);
+        ASSERT_NEAR(xy0(0, 2 * i), x0 / z0, 1e-6);
+        ASSERT_NEAR(xy0(0, 2 * i + 1), y0 / z0, 1e-6);
+
+        double x1 = P1(0, 0) * xyzw(0, 4 * i) + P1(0, 1) * xyzw(0, 4 * i + 1) + P1(0, 2) * xyzw(0, 4 * i + 2) + P1(0, 3) * xyzw(0, 4 * i + 3);
+        double y1 = P1(1, 0) * xyzw(0, 4 * i) + P1(1, 1) * xyzw(0, 4 * i + 1) + P1(1, 2) * xyzw(0, 4 * i + 2) + P1(1, 3) * xyzw(0, 4 * i + 3);
+        double z1 = P1(2, 0) * xyzw(0, 4 * i) + P1(2, 1) * xyzw(0, 4 * i + 1) + P1(2, 2) * xyzw(0, 4 * i + 2) + P1(2, 3) * xyzw(0, 4 * i + 3);
+
+        ASSERT_NEAR(xy1(0, 2 * i), x1 / z1, 1e-6);
+        ASSERT_NEAR(xy1(0, 2 * i + 1), y1 / z1, 1e-6);
     }
 }
 
+INSTANTIATE_TEST_CASE_P(StdSynthScenes,
+                        DltTriangulationProjective,
+                        testing::Values(new SphereSceneCreator(), new CubeSceneCreator()));
